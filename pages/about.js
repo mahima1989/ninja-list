2da9a677b1f9a90fe7fbcbaf93760a3e3114405f
mahima1989 <mahimarob13@gmail.com>
2021-10-11 19:17:41 +0530

    intial

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+const About =()=>{
+
+    return(<>
+        <Head>
+      <title>About</title>
+      <meta name="keywords" content="ninjas"/>
+    </Head>
+
+        <div>
+            <h1>About</h1>
+            <p>lorem kjhfkjfkwflkffjf chsvcjfgjfbwJF</p>
+            <p>lorem kjhfkjfkwflkffjf chsvcjfgjfbwJF</p>
+        </div>
+        </>
+    );
+
+}
+
+export default About;
